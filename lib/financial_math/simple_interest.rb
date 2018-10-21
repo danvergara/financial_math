f13e5dcb9e16d89780cb537553d5fd4da6674179
Daniel Omar Vergara <daniel.omar.vergara@gmail.com>
2018-10-20 23:40:44 -0500

    change periods_in_days to factor, periods divided by @frequency

--- lib/financial_math/simple_interest.rb
+++ lib/financial_math/simple_interest.rb
@@ -5,6 +5,8 @@ module FinancialMath
     def initialize(args)
       @present_value = args.fetch(:present_value, 0)
       @periods = args.fetch(:periods, 0)
+      # frequency of conversions
+      @frequency = args.fetch(:frequency, 1.0)
       @interest_rate = args.fetch(:interest_rate, 0)
       @future_value = args.fetch(:future_value, 0)
       @bank_discount_rate = args.fetch(:bank_discount_rate, 0)
@@ -19,45 +21,37 @@ module FinancialMath
     end
 
     def future_value
-      @future_value = (@present_value * (1 + periods * @interest_rate)).round(2)
-    end
-
-    def future_value_in_days
-      @future_value = @present_value * (1 + @interest_rate * periods_in_days)
+      @future_value = @present_value * (1 + @interest_rate * factor)
       @future_value.round(2)
     end
 
     def present_value
-      @present_value = (@future_value / (1 + periods * @interest_rate)).round(2)
-    end
-
-    def present_value_in_days
-      @present_value = @future_value / (1 + @interest_rate * periods_in_days)
+      @present_value = @future_value / (1 + @interest_rate * factor)
       @present_value.round(2)
     end
 
-    def rational_discount_in_days
-      (@present_value * @interest_rate * @periods_in_days).round(2)
+    def rational_discount
+      (@present_value * @interest_rate * factor).round(2)
     end
 
-    def bank_discount_in_days
-      (@future_value * @bank_discount_rate * @periods_in_days).round(2)
+    def bank_discount
+      (@future_value * @bank_discount_rate * factor).round(2)
     end
 
     def interest_rate_given_discount_rate
-      divisor = 1 - @bank_discount_rate * periods_in_days
+      divisor = 1 - @bank_discount_rate * factor
       @interest_rate = (@bank_discount_rate / divisor).round(2)
     end
 
     def discount_rate_given_interest_rate
-      divisor = 1 + @interest_rate * periods_in_days
+      divisor = 1 + @interest_rate * factor
       @bank_discount_rate = (@interest_rate / divisor).round(2)
     end
 
     private
 
-    def periods_in_days
-      periods / 360.0
+    def factor
+      periods / @frequency.to_f
     end
   end
 end
--- spec/simple_interest_spec.rb
+++ spec/simple_interest_spec.rb
@@ -6,9 +6,10 @@ RSpec.describe FinancialMath::SimpleInterest do
   end
 
   it 'is the correct future value in days' do
-    args = { present_value: 1500.0, interest_rate: 0.45, periods: 73 }
+    args = { present_value: 1500.0, interest_rate: 0.45, periods: 73,
+             frequency: 360 }
     simple_interest = FinancialMath::SimpleInterest.new(args)
-    expect(simple_interest.future_value_in_days).to eql(1636.88)
+    expect(simple_interest.future_value).to eql(1636.88)
   end
 
   it 'is the correct present value' do
@@ -18,19 +19,20 @@ RSpec.describe FinancialMath::SimpleInterest do
   end
 
   it 'is the correct present value in days' do
-    args = { future_value: 100_000.0, interest_rate: 0.415, periods: 55 }
+    args = { future_value: 100_000.0, interest_rate: 0.415, periods: 55,
+             frequency: 360 }
     simple_interest = FinancialMath::SimpleInterest.new(args)
-    expect(simple_interest.present_value_in_days).to eql(94_037.75)
+    expect(simple_interest.present_value).to eql(94_037.75)
   end
 
   it 'is the correct interest rate given the discount rate' do
-    args = { bank_discount_rate: 0.36, periods: 200 }
+    args = { bank_discount_rate: 0.36, periods: 200, frequency: 360 }
     simple_interest = FinancialMath::SimpleInterest.new(args)
     expect(simple_interest.interest_rate_given_discount_rate).to eql(0.45)
   end
 
   it 'is the correct discount rate given the interest rate' do
-    args = { interest_rate: 0.45, periods: 200 }
+    args = { interest_rate: 0.45, periods: 200, frequency: 360 }
     simple_interest = FinancialMath::SimpleInterest.new(args)
     expect(simple_interest.discount_rate_given_interest_rate).to eql(0.36)
   end
