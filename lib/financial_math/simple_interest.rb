57414dd8652a3273c86ac4ce20800d400152e6de
Daniel Omar Vergara <daniel.omar.vergara@gmail.com>
2019-02-26 14:43:08 -0600

    SimpleInterest sub-module, using functional prog instead of OOP

--- lib/financial_math/simple_interest.rb
+++ lib/financial_math/simple_interest.rb
@@ -1,57 +1,39 @@
 module FinancialMath
-  class SimpleInterest
-    attr_reader :periods
+  module SimpleInterest
+    @discount = ->(present_value, future_value) { future_value - present_value }
 
-    def initialize(args)
-      @present_value = args.fetch(:present_value, 0)
-      @periods = args.fetch(:periods, 0)
-      # frequency of conversions
-      @frequency = args.fetch(:frequency, 1.0)
-      @interest_rate = args.fetch(:interest_rate, 0)
-      @future_value = args.fetch(:future_value, 0)
-      @bank_discount_rate = args.fetch(:bank_discount_rate, 0)
+    def factor(periods: 0, frequency: 1)
+      periods / frequency.to_f
     end
 
-    def bank_discount_rate
-      @bank_discount_rate = (discount / future_value).round(2)
+    def future_value(present_value:, interest_rate:, periods: 0, frequency: 1)
+      factor_val = factor(periods: periods, frequency: frequency)
+      (present_value * (1 + interest_rate * factor_val)).round(2)
     end
 
-    def discount
-      (@future_value - @present_value).round(2)
+    def present_value(future_value:, interest_rate:, periods: 0, frequency: 1)
+      factor_val = factor(periods: periods, frequency: frequency)
+      (future_value / (1 + interest_rate * factor_val)).round(2)
     end
 
-    def future_value
-      @future_value = @present_value * (1 + @interest_rate * factor)
-      @future_value.round(2)
+    def interest_rate(discount_rate:, periods: 0, frequency: 1)
+      divisor = 1 - discount_rate * factor(periods: periods, frequency: frequency)
+      (discount_rate / divisor).round(2)
     end
 
-    def present_value
-      @present_value = @future_value / (1 + @interest_rate * factor)
-      @present_value.round(2)
+    def discount_rate(interest_rate:, periods: 0, frequency: 1)
+      divisor = 1 + interest_rate * factor(periods: periods, frequency: frequency)
+      (interest_rate / divisor).round(2)
     end
 
-    def rational_discount
-      (@present_value * @interest_rate * factor).round(2)
+    def rational_discount(present_value:, interest_rate:, periods: 0, frequency: 1)
+      factor_val = factor(periods: periods, frequency: frequency)
+      (present_value * interest_rate * factor_val).round(2)
     end
 
-    def bank_discount
-      (@future_value * @bank_discount_rate * factor).round(2)
-    end
-
-    def interest_rate_given_discount_rate
-      divisor = 1 - @bank_discount_rate * factor
-      @interest_rate = (@bank_discount_rate / divisor).round(2)
-    end
-
-    def discount_rate_given_interest_rate
-      divisor = 1 + @interest_rate * factor
-      @bank_discount_rate = (@interest_rate / divisor).round(2)
-    end
-
-    private
-
-    def factor
-      periods / @frequency.to_f
+    def bank_discount(future_value:, discount_rate:, periods: 0, frequency: 1)
+      factor_val = factor(periods: periods, frequency: frequency)
+      (future_value * discount_rate * factor_val).round(2)
     end
   end
 end
