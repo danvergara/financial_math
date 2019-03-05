5161fd4e105855e8561f134c1e15bdcda62fdda9
Daniel Omar Vergara <daniel.omar.vergara@gmail.com>
2019-03-05 01:09:35 -0600

    CompoundInterest sub-module, using functional prog instead of OOP

--- lib/financial_math/simple_interest.rb
+++ lib/financial_math/simple_interest.rb
@@ -1,11 +1,5 @@
 module FinancialMath
   module SimpleInterest
-    @discount = ->(present_value, future_value) { future_value - present_value }
-
-    def factor(periods: 0, frequency: 1)
-      periods / frequency.to_f
-    end
-
     def future_value(present_value:, interest_rate:, periods: 0, frequency: 1)
       factor_val = factor(periods: periods, frequency: frequency)
       (present_value * (1 + interest_rate * factor_val)).round(2)
@@ -17,16 +11,19 @@ module FinancialMath
     end
 
     def interest_rate(discount_rate:, periods: 0, frequency: 1)
-      divisor = 1 - discount_rate * factor(periods: periods, frequency: frequency)
+      divisor = 1 - discount_rate * factor(periods: periods,
+                                           frequency: frequency)
       (discount_rate / divisor).round(2)
     end
 
     def discount_rate(interest_rate:, periods: 0, frequency: 1)
-      divisor = 1 + interest_rate * factor(periods: periods, frequency: frequency)
+      divisor = 1 + interest_rate * factor(periods: periods,
+                                           frequency: frequency)
       (interest_rate / divisor).round(2)
     end
 
-    def rational_discount(present_value:, interest_rate:, periods: 0, frequency: 1)
+    def rational_discount(present_value:, interest_rate:,
+                          periods: 0, frequency: 1)
       factor_val = factor(periods: periods, frequency: frequency)
       (present_value * interest_rate * factor_val).round(2)
     end
@@ -34,6 +31,12 @@ module FinancialMath
     def bank_discount(future_value:, discount_rate:, periods: 0, frequency: 1)
       factor_val = factor(periods: periods, frequency: frequency)
       (future_value * discount_rate * factor_val).round(2)
+    end
+
+    private
+
+    def factor(periods: 0, frequency: 1)
+      periods / frequency.to_f
     end
   end
 end
